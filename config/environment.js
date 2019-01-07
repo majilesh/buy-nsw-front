60a948ffd7e953226f460c5c82c5b7348f7769d0
Arman <arman.zrb@gmail.com>
2019-01-07 14:36:41 +1100

    Running Ember with "_" prefix

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'buy-nsw-ember',
     environment,
-    rootURL: '/',
+    rootURL: '/_/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
