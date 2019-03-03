9bce267688f4fcfc9398d8c29e9497a3725ef108
Arman <arman.zrb@gmail.com>
2019-03-03 17:27:37 +1100

    Adding csrf protection

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,13 +2,15 @@ import DS from 'ember-data';
 import Ember from 'ember';
 import { computed } from '@ember/object';
 import { underscore } from '@ember/string';
+import {inject as service} from '@ember/service';
 
 const { String: { pluralize } } = Ember;
 
 export default DS.RESTAdapter.extend({
-  headers: computed(function() {
+  auth: service(),
+  headers: computed('auth.csrfToken', function() {
     return {
-      "X-CSRF-Token": document.querySelector("meta[name='csrf-token']").getAttribute("content")
+      "X-CSRF-Token": this.get('auth.csrfToken'),
     };
   }),
   namespace: '/api',
