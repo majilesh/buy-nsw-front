aab9df4a015a1f10249831c51f34113f617901d3
Arman <arman.zrb@gmail.com>
2019-03-05 16:51:01 +1100

    minor lint fixes

--- app/routes/telco-form.js
+++ app/routes/telco-form.js
@@ -1,17 +1,16 @@
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
 import { on } from '@ember/object/evented';
-import { computed } from '@ember/object';
 
 export default Route.extend({
   model(params) {
     var store = this.store;
     return RSVP.hash({
       form_key: params.form_key,
-      form_name: params.form_key.replace(/\-/g, ' ').replace(/\b[a-z]/g, (s) => s.toUpperCase()),
+      form_name: params.form_key.replace(/-/g, ' ').replace(/\b[a-z]/g, (s) => s.toUpperCase()),
       component_name: params.form_key + '-form',
       product: store.query('telco-product', {
-        category: params.form_key.replace(/\-/g, '_')
+        category: params.form_key.replace(/-/g, '_')
       }).then(function(products) {
         var product = products.get('firstObject');
         if(product) {
