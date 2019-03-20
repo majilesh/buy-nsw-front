41cad635dd248507dd10c5bf833e95e6a4e83699
Arman <arman.zrb@gmail.com>
2019-03-20 14:32:13 +1100

    Fixing the error showing by default

--- app/components/ds/radio-button.js
+++ app/components/ds/radio-button.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  actions: {
+    selectRadio() {
+      this.set('field', this.get('value'));
+      this.onChange();
+    }
+  }
 });
