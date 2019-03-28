3e48631127770d4ec1106154cabf71fd36e31892
Arman <arman.zrb@gmail.com>
2019-03-28 11:07:50 +1100

    Handling scroll to top using ember-router-scroll

--- app/router.js
+++ app/router.js
@@ -1,18 +1,10 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
+import RouterScroll from 'ember-router-scroll';
 
-const Router = EmberRouter.extend({
+const Router = EmberRouter.extend(RouterScroll, {
   location: config.locationType,
   rootURL: config.rootURL,
-
-  init() {
-    this._super(...arguments);
-    this.on('routeDidChange', transition => {
-      if (!this.get('fastboot.isFastBoot')) {
-        window.scrollTo(0, 0);
-      }
-    });
-  }
 });
 
 Router.map(function() {
--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,8 @@ module.exports = function(environment) {
     modulePrefix: 'buy-nsw-ember',
     environment,
     rootURL: '/_/',
-    locationType: 'auto',
+    locationType: 'router-scroll',
+    historySupportMiddleware: true,
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
