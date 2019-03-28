c6323e8ccf5693fc5a0c4c43c96949b79413f9d5
Arman <arman.zrb@gmail.com>
2019-03-29 09:48:25 +1100

    Adding loading indicator

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,9 +1,23 @@
 import Route from '@ember/routing/route';
+import Ember from 'ember';
+const {$} = Ember;
 
 export default Route.extend({
   actions: {
-    error: function(error) {
+    loading: function(transition, originRoute) {
+      $('.overlay').show();
+
+      transition.promise.finally(() => {
+        $('.overlay').hide();
+      });
+
       return true;
+    },
+    didTransition: function() {
+      $('.overlay').hide();
     }
+  },
+  error: function(error) {
+    return true;
   }
 });
