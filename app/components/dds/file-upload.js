3a43824550ea3d26b8d587855a66eb1a29f6ac8a
Arman <arman.zrb@gmail.com>
2019-06-07 13:11:58 +1000

    Move the file upload to a component

---
+++ app/components/dds/file-upload.js
@@ -0,0 +1,16 @@
+import Component from '@ember/component';
+import Ember from 'ember';
+import { task } from 'ember-concurrency';
+
+const { get, set } = Ember;
+
+export default Component.extend({
+  uploadFile: task(function * (file) {
+    let response = yield file.upload('/api/documents/documents/');
+  }).maxConcurrency(3).enqueue(),
+  actions: {
+    uploadDocument(file) {
+      get(this, 'uploadFile').perform(file);
+    }
+  }
+});
