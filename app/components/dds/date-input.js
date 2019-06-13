bc23e96f75136773f8ea27a438d6ad793cb5f813
Arman <arman.zrb@gmail.com>
2019-06-13 11:23:24 +1000

    Adding date fields to insurance form

--- app/components/dds/date-input.js
+++ app/components/dds/date-input.js
@@ -1,4 +1,42 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import moment from 'moment';
 
 export default Component.extend({
+  present: computed('field', function () {
+    let value = this.get('field');
+    if (value == "" || value == undefined) {
+      return false; 
+    }
+    return true
+  }),
+
+  valid: computed('field', 'present', function () {
+    if(this.get('present') == false) {
+      return false;
+    }
+    let value = this.get('field');
+    return value.match(/^\d{4}-\d{1,2}-\d{1,2}$/) != null &&
+           moment.parseZone(value, 'YYYY-MM-DD').isValid();
+  }),
+
+  inRange: computed('field', 'valid', function () {
+    if(this.get('valid') == false) {
+      return false;
+    }
+    let value = this.get('field');
+    let m = moment.parseZone(value, 'YYYY-MM-DD');
+    return m.isBetween(this.get('min'), this.get('max'));
+  }),
+
+  min: computed(function() {
+    return moment().format('YYYY-MM-DD');
+  }),
+
+  max: '2050-01-01',
+
+  inputType: computed('field', function () {
+    let value = this.get('field');
+    return typeof value;
+  }),
 });
--- app/models/seller-form/insurance.js
+++ app/models/seller-form/insurance.js
@@ -1,7 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  financial_statement_ids: DS.attr('json'),
   professional_indemnity_certificate_ids: DS.attr('json'),
+  product_liability_certificate_ids: DS.attr('json'),
   workers_compensation_certificate_ids: DS.attr('json'),
+
+  professional_indemnity_certificate_expiry: DS.attr('string'),
+  product_liability_certificate_expiry: DS.attr('string'),
+  workers_compensation_certificate_expiry: DS.attr('string'),
 });
