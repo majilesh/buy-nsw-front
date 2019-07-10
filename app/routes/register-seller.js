3121753b2d4053add3d40049dc9b390776f8ed23
Arman <arman.zrb@gmail.com>
2019-07-10 12:57:47 +1000

    redirect user to application page from register page if user already started it

--- app/routes/register-seller.js
+++ app/routes/register-seller.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model(params) {
+    return  this.store.queryRecord('user', {current: true});
+  },
+  afterModel(model, transition) {
+    if (model.hasSeller) {
+      this.transitionTo('supplier-application');
+    }
+  }
 });
