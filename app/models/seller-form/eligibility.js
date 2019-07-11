b9f24782314b4acb102e0e5d70470151f2ac2542
Arman <arman.zrb@gmail.com>
2019-07-11 14:27:35 +1000

    Adding eligibility error

--- app/models/seller-form/eligibility.js
+++ app/models/seller-form/eligibility.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 import { buildValidations, validator } from 'ember-cp-validations';
+import { computed } from '@ember/object';
 
 const Validations = buildValidations({
   offers_cloud: {
@@ -12,6 +13,16 @@ const Validations = buildValidations({
       validator('presence', true),
     ]
   },
+  eligible: {
+    validators: [
+      validator('inline', {
+        message: 'To become an eligible supplier on buy.nsw you have to offer one of the above',
+        validate(value, options, model, attributes) {
+          return value == true || options.message;
+        }
+      }),
+    ]
+  },
 });
 
 export default DS.Model.extend(Validations, {
@@ -20,4 +31,8 @@ export default DS.Model.extend(Validations, {
   offers_cloud: DS.attr('string'),
   govdc: DS.attr('string'),
   services: DS.attr('json'),
+
+  eligible: computed('offers_cloud', 'govdc', function() {
+    return this.get('offers_cloud') == 'true' || this.get('govdc') == 'true';
+  }),
 });
