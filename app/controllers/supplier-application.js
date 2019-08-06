94474fa385918d63060cdd2653be324f22d785f7
Arman <arman.zrb@gmail.com>
2019-08-06 16:00:18 +1000

    Bug fixes from test on 6 Aug

--- app/controllers/supplier-application.js
+++ app/controllers/supplier-application.js
@@ -18,35 +18,35 @@ export default Controller.extend({
       var controller = this;
       this.model.seller.submit().then(function() {
         controller.send('refresh');
-      });
+      }).finally(() => $('.overlay').hide());
     },
     cancelProfile() {
       $('.overlay').show();
       var controller = this;
       this.model.seller.cancel().then(function(response) {
         controller.send('refresh');
-      });
+      }).finally(() => $('.overlay').hide());
     },
     withdrawSubmission() {
       $('.overlay').show();
       var controller = this;
       this.model.seller.withdraw().then(function(response) {
         controller.send('refresh');
-      });
+      }).finally(() => $('.overlay').hide());
     },
     reviseProfile() {
       $('.overlay').show();
       var controller = this;
       this.model.seller.revise().then(function(response) {
         controller.send('refresh');
-      });
+      }).finally(() => $('.overlay').hide());
     },
     amendProfile() {
       $('.overlay').show();
       var controller = this;
       this.model.seller.startAmendment().then(function(response) {
         controller.send('refresh');
-      });
+      }).finally(() => $('.overlay').hide());
     },
   }
 });
--- app/models/seller-form/company-profile.js
+++ app/models/seller-form/company-profile.js
@@ -17,7 +17,11 @@ const Validations = buildValidations({
       validator('presence', true),
       validator('format', {
         type: 'url'
-      })
+      }),
+      validator('format', {
+        regex: /^(http:\/\/)|(https:\/\/)/,
+        message: 'URL must start with http:// or https://',
+      }),
     ]
   },
   linkedin_url: {
@@ -25,7 +29,12 @@ const Validations = buildValidations({
       validator('format', {
         allowBlank: true,
         type: 'url'
-      })
+      }),
+      validator('format', {
+        allowBlank: true,
+        regex: /^(http:\/\/)|(https:\/\/)/,
+        message: 'URL must start with http:// or https://',
+      }),
     ]
   },
   facebook_url: {
@@ -33,7 +42,12 @@ const Validations = buildValidations({
       validator('format', {
         allowBlank: true,
         type: 'url'
-      })
+      }),
+      validator('format', {
+        allowBlank: true,
+        regex: /^(http:\/\/)|(https:\/\/)/,
+        message: 'URL must start with http:// or https://',
+      }),
     ]
   },
   youtube_url: {
@@ -41,7 +55,12 @@ const Validations = buildValidations({
       validator('format', {
         allowBlank: true,
         type: 'url'
-      })
+      }),
+      validator('format', {
+        allowBlank: true,
+        regex: /^(http:\/\/)|(https:\/\/)/,
+        message: 'URL must start with http:// or https://',
+      }),
     ]
   },
   twitter_url: {
@@ -49,7 +68,12 @@ const Validations = buildValidations({
       validator('format', {
         allowBlank: true,
         type: 'url'
-      })
+      }),
+      validator('format', {
+        allowBlank: true,
+        regex: /^(http:\/\/)|(https:\/\/)/,
+        message: 'URL must start with http:// or https://',
+      }),
     ]
   },
   instagram_url: {
@@ -57,7 +81,12 @@ const Validations = buildValidations({
       validator('format', {
         allowBlank: true,
         type: 'url'
-      })
+      }),
+      validator('format', {
+        allowBlank: true,
+        regex: /^(http:\/\/)|(https:\/\/)/,
+        message: 'URL must start with http:// or https://',
+      }),
     ]
   }
 });
