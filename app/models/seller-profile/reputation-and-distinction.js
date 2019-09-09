698d2d60d45d6ee6aa5ba27bf699002a99f8221b
Arman <arman.zrb@gmail.com>
2019-09-09 15:07:25 +1000

    Adding scaffold for reputation form

--- app/models/seller-profile/reputation-and-distinction.js
+++ app/models/seller-profile/reputation-and-distinction.js
@@ -7,4 +7,11 @@ const Validations = buildValidations({
 });
 
 export default Model.extend(Validations, {
+  signal: DS.attr('number', { defaultValue: 0 }),
+  accreditations: DS.attr('json'),
+  accreditation_document_ids: DS.attr('json'),
+  licenses: DS.attr('json'),
+  license_document_ids: DS.attr('json'),
+  awards: DS.attr('json'),
+  engagements: DS.attr('json'),
 });
