9467432b39f9355a31f629fa589b1f8d37019a3c
Arman <arman.zrb@gmail.com>
2019-09-10 11:33:42 +1000

    seller checks his profile as buyer

--- app/controllers/seller-profile.js
+++ app/controllers/seller-profile.js
@@ -1,6 +1,11 @@
 import BaseController from './base-controller';
 import { inject } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default BaseController.extend({
   fileService: inject(),
+  buyerView: computed('auth.user', 'auth.isBuyer', 'model.profile.seller_id', function () {
+    return this.get('auth.user') && ( this.get('auth.isBuyer') || (
+      this.get('auth.isSeller') && this.get('auth.user').seller_id == this.get('model.profile').id ));
+  })
 });
