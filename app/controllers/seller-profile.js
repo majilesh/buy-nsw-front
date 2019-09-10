3bc0b5ba8ce8880dfeec5e2f21a8a53d8fa3aad9
Arman <arman.zrb@gmail.com>
2019-09-10 12:59:43 +1000

    Handle buyer registeration check in profile view

--- app/controllers/seller-profile.js
+++ app/controllers/seller-profile.js
@@ -5,7 +5,9 @@ import { computed } from '@ember/object';
 export default BaseController.extend({
   fileService: inject(),
   buyerView: computed('auth.user', 'auth.isBuyer', 'model.profile.seller_id', function () {
-    return this.get('auth.user') && ( this.get('auth.isBuyer') || (
-      this.get('auth.isSeller') && this.get('auth.user').seller_id == this.get('model.profile').id ));
+    return this.get('auth.user') && (
+      this.get('auth.isBuyer') && this.get('auth.user').can_buy ||
+      this.get('auth.isSeller') && (this.get('auth.user').seller_id == this.get('model.profile').id)
+    );
   })
 });
