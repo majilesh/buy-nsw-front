fa026ebb74acf8adec594232db434d38360b7e40
Arman <arman.zrb@gmail.com>
2019-09-16 10:02:50 +1000

    bug fix on public supplier page

--- app/controllers/seller-profile.js
+++ app/controllers/seller-profile.js
@@ -4,10 +4,10 @@ import { computed } from '@ember/object';
 
 export default BaseController.extend({
   fileService: inject(),
-  buyerView: computed('auth.user', 'auth.isBuyer', 'model.profile.id', function () {
+  buyerView: computed('auth.user', 'auth.isBuyer', 'model.seller.id', function () {
     return this.get('auth.user') && (
       this.get('auth.isBuyer') && this.get('auth.user').can_buy ||
-      this.get('auth.isSeller') && this.get('auth.user').seller_id == this.get('model.profile.id')
+      this.get('auth.isSeller') && this.get('auth.user').seller_id == this.get('model.seller.id')
     );
   })
 });
