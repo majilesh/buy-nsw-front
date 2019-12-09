31502124508e2172ac3464a08660e79c31560ff3
Arman <arman.zrb@gmail.com>
2019-12-09 14:45:53 +1100

    Add auto cache invalidation for cloudfront to deploy process

--- config/deploy.js
+++ config/deploy.js
@@ -18,5 +18,18 @@ module.exports = function(deployTarget) {
     revisionKey: 'latest',
   };
 
+  var distributionIds = {
+    testing: 'E1QZLKL9XK1Y54',
+    staging: 'E1A0WUF4DYWWZD',
+    production: ['E268RMHQPA3RN8','E205PQV4ZJNX1S']
+  };
+
+  ENV['cloudfront'] = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    objectPaths: ['/index.html', '/', '/ict', '/ict/*'],
+    distribution: distributionIds[deployTarget],
+  };
+
   return ENV;
 };
