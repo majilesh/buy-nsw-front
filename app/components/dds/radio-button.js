1731a32a94d44782fc0175119828e9ada6d693a1
Adrian Hall <adohall@gmail.com>
2019-12-12 11:50:55 +1100

    SO-426 - style select boxes and radio inputs

--- app/components/dds/radio-button.js
+++ app/components/dds/radio-button.js
@@ -3,6 +3,7 @@ import layout from '../../templates/components/dds/radio-button';
 
 export default Component.extend({
   layout,
+  classNameBindings: ['isSimple:radio-pair:au-control-input'],
   actions: {
     selectRadio() {
       this.set('field', this.get('value'));
