2fbb224ce16cd9caf68ebb784a8e009082b3cb8a
Adrian Hall <adohall@gmail.com>
2020-01-10 16:55:04 +1100

    SO-431 - supplier account legal disclosures

--- app/components/dds/text-area.js
+++ app/components/dds/text-area.js
@@ -4,6 +4,7 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
   layout,
+  classNameBindings: ['label-text:textarea-pair'],
   displayError: computed('showError', 'hasChanged', 'field', function() {
     return this.get('showError') || this.get('hasChanged');
   }),
