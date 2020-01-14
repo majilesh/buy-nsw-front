ecafe22751273fd1cd618d6a93b83b543ac3e5a6
Adrian Hall <adohall@gmail.com>
2020-01-14 11:39:53 +1100

    SO-426 - define 'signal' for address changes

--- app/models/seller-account/business-address.js
+++ app/models/seller-account/business-address.js
@@ -53,7 +53,6 @@ export default DS.Model.extend(Validations, {
   }),
 
   corporate_structure: DS.attr('string'),
-  //same_as_above: DS.attr('boolean'),
-  show_address: DS.attr('checked', { defaultValue: 'checked' })
-  //signal: DS.attr('number', { defaultValue: 0 }),
+  show_address: DS.attr('checked', { defaultValue: 'checked' }),
+  signal: DS.attr('number', { defaultValue: 0 }),
 });
