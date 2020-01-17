c4023496dba4a267d3567224cb84c0464de711fb
Adrian Hall <adohall@gmail.com>
2020-01-17 12:29:23 +1100

    SO-429 - stage 3

--- app/components/seller-account/insurance-document-view.js
+++ app/components/seller-account/insurance-document-view.js
@@ -1,4 +1,22 @@
 import Component from '@ember/component';
+import { inject } from '@ember/service';
 
 export default Component.extend({
+  fileService: inject(),
+  actions: {
+    getDocuments() {
+      let self = this;
+      this.get('bjax').request('/api/events/events/documents_requested', {
+        method: 'POST',
+        headers: {
+          "X-CSRF-Token": this.get('auth.csrfToken'),
+        },
+        data: {
+          seller_id: self.get('model.seller.id')
+        }
+      }).then(()=>{
+        self.set('showDocumentDownloads', true);
+      })
+    }
+  }
 });
