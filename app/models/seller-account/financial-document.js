de83064f2a70e3e0b8a76e24789c9b6f6c8d3b4c
Adrian Hall <adohall@gmail.com>
2020-01-23 13:57:31 +1100

    SO-430 - stage 1

--- app/models/seller-account/financial-document.js
+++ app/models/seller-account/financial-document.js
@@ -1,6 +1,34 @@
 import DS from 'ember-data';
-const { Model } = DS;
+import { buildValidations, validator } from 'ember-cp-validations';
+import { computed } from '@ember/object';
 
-export default Model.extend({
+const Validations = buildValidations({
+  financial_statement_confirmed_valid: {
+    validators: [
+      validator('inline', {
+        message: 'Please confirm the validity of attached documents',
+        validate(value, options, model, attributes) { // eslint-disable-line no-unused-vars
+          return value == true || options.message;
+        }
+      }),
+    ]
+  },
+});
 
+export default DS.Model.extend(Validations, {
+  status: DS.attr('string'),
+  apiErrors: DS.attr('json'),
+
+  financial_statement_ids: DS.attr('json'),
+  financial_statement_confirmed: DS.attr('boolean'),
+  financial_statement_confirmed_valid: computed(
+    'financial_statement_ids',
+    'financial_statement_ids.[]',
+    'financial_statement_confirmed',
+    function() {
+      return this.get('financial_statement_ids').length == 0 ||
+             this.get('financial_statement_confirmed') == true;
+  }),
+
+  signal: DS.attr('number', { defaultValue: 0 }),
 });
