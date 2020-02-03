07df1a72f3ab93c7ba67078db4746cb854c0cfee
Arman <arman.zrb@gmail.com>
2020-02-03 13:15:16 +1100

    removing the unused var

--- app/components/account-nav.js
+++ app/components/account-nav.js
@@ -5,9 +5,8 @@ export default Component.extend({
   tagName: 'nav',
   classNames: ['sapphire-nav'],
   overlay: inject(),
-  router: inject(),
   actions: {
-    linkClicked(step) {
+    linkClicked() {
       if(this.get('underEdit')) {
         this.get('overlay').showError('Please Save or Cancel your changes before leaving this page.');
       }
